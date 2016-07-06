55e0fc7edccc1d97639477a6e93670ff1204bce0
mgreg90 <mgregory8219@gmail.com>
2016-07-06 04:22:14 -0400

    added title and beginning of projects list

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -5,19 +5,19 @@ function showHome() {
 };
 
 function showBio() {
-  document.getElementById('bio-container').style.display = 'block';
+  document.getElementById('bio-container').style.display = 'flex';
   document.getElementById('projects-container').style.display = 'none';
   document.getElementById('contact-container').style.display = 'none';
 };
 
 function showProjects() {
   document.getElementById('bio-container').style.display = 'none';
-  document.getElementById('projects-container').style.display = 'block';
+  document.getElementById('projects-container').style.display = 'flex';
   document.getElementById('contact-container').style.display = 'none';
 };
 
 function showContact() {
   document.getElementById('bio-container').style.display = 'none';
   document.getElementById('projects-container').style.display = 'none';
-  document.getElementById('contact-container').style.display = 'block';
+  document.getElementById('contact-container').style.display = 'flex';
 };
