4849cb3d1cef5e2c6ad9e1960bc40aa575afe512
mgreg90 <mgregory8219@gmail.com>
2016-07-06 03:03:42 -0400

    added js to have buttons hide and show different divs

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,4 +1,23 @@
+function showHome() {
+  document.getElementById('bio-container').style.display = 'none';
+  document.getElementById('projects-container').style.display = 'none';
+  document.getElementById('contact-container').style.display = 'none';
+};
+
 function showBio() {
-  document.getElementById('bio').style.color = "red";
-  console.log('worked!');
+  document.getElementById('bio-container').style.display = 'block';
+  document.getElementById('projects-container').style.display = 'none';
+  document.getElementById('contact-container').style.display = 'none';
+};
+
+function showProjects() {
+  document.getElementById('bio-container').style.display = 'none';
+  document.getElementById('projects-container').style.display = 'block';
+  document.getElementById('contact-container').style.display = 'none';
+};
+
+function showContact() {
+  document.getElementById('bio-container').style.display = 'none';
+  document.getElementById('projects-container').style.display = 'none';
+  document.getElementById('contact-container').style.display = 'block';
 };
