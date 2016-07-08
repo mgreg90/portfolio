6dcf26be8a9616c165c4baaa0b8922afa53cfdb2
mgreg90 <mgregory8219@gmail.com>
2016-07-08 05:10:33 -0400

    changed over to using jquery

--- public/javascripts/home-jquery.js
+++ public/javascripts/home-jquery.js
@@ -1,13 +1,51 @@
-  // var button = $(".indiv-project-button");
-  // console.log(button);
-  // button.click(highlightProjectButton(button));
-$(document).ready(function(){
-  function highlightProjectButton() {
-    alert("function: highlightProjectButton");
-    // button.css({"background-color": "yellow"});
-  };
-  function unHighlightProjectButton() {
-    alert("function: unHighlightProjectButton");
-  };
-  // $("div.indiv-project-button").click(highlightProjectButton());
-});
+function renderLanding() {
+  $('#landing-container').show();
+  $('#bio-container').hide();
+  $('#projects-container').hide();
+  $('#contact-container').hide();
+  pushDownNavBar();
+}
+
+function renderBio() {
+  $('#landing-container').hide();
+  $('#bio-container').show();
+  $('#projects-container').hide();
+  $('#contact-container').hide();
+  pushUpNavBar();
+}
+
+function renderProjects() {
+  $('#landing-container').hide();
+  $('#bio-container').hide();
+  $('#projects-container').show();
+  $('#contact-container').hide();
+  pushUpNavBar();
+}
+
+function renderContact() {
+  $('#landing-container').hide();
+  $('#bio-container').hide();
+  $('#projects-container').hide();
+  $('#contact-container').show();
+  pushUpNavBar();
+}
+
+function pushUpNavBar() {
+  $('.navbar-bottom').css('margin-bottom', '20px');
+}
+
+function pushDownNavBar() {
+  $('.navbar-bottom').css('margin-bottom', '0');
+}
+
+function runPage() {
+  // On load stuff
+  renderLanding();
+  // On event stuff
+  $('#bio').click(renderBio);
+  $('#projects').click(renderProjects);
+  $('#contact').click(renderContact);
+  $('#nav-logo').click(renderLanding);
+}
+
+$(document).ready(runPage);
