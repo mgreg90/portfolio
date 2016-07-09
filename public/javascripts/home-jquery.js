8fb2a9d73d3137ec6f54a1afc27cf4b5a8b8fe40
mgreg90 <mgregory8219@gmail.com>
2016-07-08 23:50:47 -0400

    got slide-in slide-out working for projects. still no animation going from menu item to menu item. Bio and contact not executing animation either.

--- public/javascripts/home-jquery.js
+++ public/javascripts/home-jquery.js
@@ -1,8 +1,3 @@
-function renderBody() {
-  $('#landing-container').hide();
-  $('.body-container').show();
-}
-
 function renderLanding(innerContainersArray) {
   $('#landing-container').addClass('visible').show();
   $('.body-container').removeClass('visible').hide();
@@ -13,33 +8,70 @@ function renderLanding(innerContainersArray) {
 }
 
 function renderLandingAnimated() {
-  // $('.body-container').animate({"left":"-1000px"}, "slow");
-  $('.body-container').removeClass('visible').hide();
-  $('#landing-container').addClass('visible').show();
+  $('.page-container').animate({left: 1660}, 500, function () {
+    $('.body-container').removeClass('visible').hide();
+    $('#landing-container').addClass('visible').show();
+  });
   pushDownNavBar();
 }
 
 function renderBio() {
-  renderBody()
-  $('#bio-container').addClass('visible').show();
-  $('#projects-container').removeClass('visible').hide();
+  $('#landing-container').hide();
+  $('.body-container').show();
+  $('#bio-container').removeClass('visible').show();
+  $('#projects-container').addClass('visible').hide();
   $('#contact-container').removeClass('visible').hide();
   pushUpNavBar();
 }
 
+function renderBioAnimated() {
+  $('.body-container').show();
+  $('#bio-container').addClass('visible').show();
+  $('.page-container').animate({left: 160}, 500, function() {
+    $('#projects-container').removeClass('visible').hide();
+    $('#contact-container').removeClass('visible').hide();
+    $('#landing-container').hide();
+  })
+  pushUpNavBar();
+}
+
 function renderProjects() {
-  renderBody();
+  $('#landing-container').hide();
+  $('.body-container').show();
   $('#bio-container').removeClass('visible').hide();
   $('#projects-container').addClass('visible').show();
   $('#contact-container').removeClass('visible').hide();
   pushUpNavBar();
 }
 
+function renderProjectsAnimated() {
+  $('.body-container').show();
+  $('#projects-container').addClass('visible').show();
+  $('.page-container').animate({left: 160}, 500, function() {
+    $('#bio-container').removeClass('visible').hide();
+    $('#contact-container').removeClass('visible').hide();
+    $('#landing-container').hide();
+  })
+  pushUpNavBar();
+}
+
 function renderContact() {
-  renderBody();
+  $('#landing-container').hide();
+  $('.body-container').show();
   $('#bio-container').removeClass('visible').hide();
   $('#projects-container').removeClass('visible').hide();
   $('#contact-container').addClass('visible').show();
+  pushUpNavBar();
+}
+
+function renderContactAnimated() {
+  $('.body-container').show();
+  $('#contact-container').addClass('visible').show();
+  $('.page-container').animate({left: 160}, 500, function() {
+    $('#bio-container').removeClass('visible').hide();
+    $('#projects-container').removeClass('visible').hide();
+    $('#landing-container').hide();
+  })
   pushUpNavBar();
 }
 
@@ -58,10 +90,11 @@ function runPage() {
   ];
   // On load stuff
   renderLanding(innerContainersArray);
+  // renderProjects();
   // On event stuff
-  $('#bio').click(renderBio);
-  $('#projects').click(renderProjects);
-  $('#contact').click(renderContact);
+  $('#bio').click(renderBioAnimated);
+  $('#projects').click(renderProjectsAnimated);
+  $('#contact').click(renderContactAnimated);
   $('#nav-logo').click(renderLandingAnimated);
 }
 
